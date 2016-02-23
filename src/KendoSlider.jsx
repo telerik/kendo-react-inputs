e2cdd34de3cfb4e7558dd69927eb104d586c9e4d
Kiril Nikolov <kiril.nikolov@gmail.com>
2016-02-23 16:28:54 +0200

    feat: add Slider component basic rendering

--- src/KendoSlider.jsx
+++ src/KendoSlider.jsx
@@ -1,11 +1,52 @@
 import * as React from 'react';
 
-import util from './util';
+//import styles from '@telerik/kendo-theme-default/styles/main';
 
-import styles from '@telerik/kendo-theme-default/styles/main';
+import classnames from 'classnames';
 
-export default function KendoSlider(props) {
-    return (
-        <div {...props} className={styles.componentClass}>A Kendo UI react component{util()} </div>
-    );
+import SliderTrack from '../src/SliderTrack';
+
+import SliderTicks from '../src/SliderTicks';
+
+import SliderButton from '../src/SliderButton';
+
+export default class KendoSlider extends React.Component {
+    onIncrease = () => {
+        /*increae handler */
+    };
+
+    onDecrease = () => {
+        /*decrease handler*/
+    };
+
+    onTrackClick = () => {
+        /*track click*/
+    };
+
+    onTickClick = () => {
+        /*tick click*/
+    };
+
+    render() {
+        const classes = classnames({
+            'k-widget': true,
+            'k-slider': true,
+            'k-slider-horizontal': true,
+            'k-state-default': true
+        });
+
+        return (
+            <div {...this.props} className={classes}>
+                <div className={"k-slider-wrap k-slider-buttons"} >
+                    <SliderButton increase onClick={this.onIncrease} title="Left" />
+                    <SliderButton onClick={this.onDecrease} title="Right" />
+
+                    <SliderTicks onClick={this.onTickClick} tickCount={2} />
+
+                    <SliderTrack onClick={this.onTrackClick} />
+
+                </div>
+            </div>
+           );
+    }
 }
