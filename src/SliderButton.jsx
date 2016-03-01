3325eabce2ddc7df535686b05974dd2b4d32643c
Kiril Nikolov <kiril.nikolov@gmail.com>
2016-03-01 19:16:18 +0200

    feat(widget): add vertical rendering for the widget

--- src/SliderButton.jsx
+++ src/SliderButton.jsx
@@ -3,19 +3,22 @@ import * as React from 'react';
 import classnames from 'classnames';
 
 const SliderButton = (props) => {
+    const { increase, vertical } = props;
     const classes = classnames({
         'k-button': true,
-        'k-button-increase': props.increase,
-        'k-button-decrease': !props.increase
+        'k-button-increase': increase,
+        'k-button-decrease': !increase
     });
-    const arrowClass = classnames({
+    const spanClasses = classnames({
         'k-icon': true,
-        'k-i-arrow-e': props.increase,
-        'k-i-arrow-w': !props.increase
+        'k-i-arrow-e': increase && !vertical,
+        'k-i-arrow-w': !increase && !vertical,
+        'k-i-arrow-n': increase && vertical,
+        'k-i-arrow-s': !increase && vertical
     });
     return (
         <a className={classes}>
-            <span className={arrowClass} onClick={props.onClick}>{props.title}</span>
+            <span className={spanClasses} onClick={props.onClick}>{props.title}</span>
         </a>
     );
 };
@@ -23,7 +26,8 @@ const SliderButton = (props) => {
 SliderButton.propTypes = {
     increase: React.PropTypes.bool,
     onClick: React.PropTypes.func,
-    title: React.PropTypes.string
+    title: React.PropTypes.string,
+    vertical: React.PropTypes.bool
 };
 
 export default SliderButton;
--- test/SliderButton.jsx
+++ test/SliderButton.jsx
@@ -18,6 +18,26 @@ describe('SliderButton', () => {
         expect(result.hasClass('k-button-decrease')).toBe(true);
     });
 
+    it('should add class k-i-arrow-e when increase and not vertical', () => {
+        let result = shallow(<SliderButton increase/>);
+        expect(result.children().first().hasClass('k-i-arrow-e')).toBe(true);
+    });
+
+    it('should add class k-i-arrow-w when increase and not vertical', () => {
+        let result = shallow(<SliderButton />);
+        expect(result.children().first().hasClass('k-i-arrow-w')).toBe(true);
+    });
+
+    it('should add class k-i-arrow-n when increase and not vertical', () => {
+        let result = shallow(<SliderButton increase vertical/>);
+        expect(result.children().first().hasClass('k-i-arrow-n')).toBe(true);
+    });
+
+    it('should add class k-i-arrow-s when increase and not vertical', () => {
+        let result = shallow(<SliderButton vertical/>);
+        expect(result.children().first().hasClass('k-i-arrow-s')).toBe(true);
+    });
+
     it('should render child span element', () => {
         let result = shallow(<SliderButton />);
         expect(result.children().first().type()).toEqual('span');
