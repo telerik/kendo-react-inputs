03c00bd77c2ce8b9bcdab0adbf911e47d4371015
Kiril Nikolov <kiril.nikolov@gmail.com>
2016-03-08 14:10:50 +0200

    docs: update demo

--- examples/basic.jsx
+++ examples/basic.jsx
@@ -4,22 +4,40 @@ import KendoSlider from '../src/KendoSlider';
 
 const state = {
     max: 10,
-    min: 0,
-    value: 6,
-    step: 4,
+    min: -4,
+    value: 4,
+    step: 2,
+    fixedTickWidth: 10,
+    decreaseButtonTitle: "Dec",
+    increaseButtonTitle: "Inc",
+    buttons: true,
+    tickPlacement: "both",
     onChange: function(e) {
         state.value = e.value;
         render();
+    },
+    title: function(value) {
+        if (value % 2 === 0) {
+            return 'even';
+        }
+        return 'odd';
     }
 };
 const render = () => {
     ReactDOM.render(
         <KendoSlider
-            max={state.max}
-            min={state.min}
+            buttons = {state.buttons}
+            decreaseButtonTitle = {state.decreaseButtonTitle}
+            fixedTickWidth = {state.fixedTickWidth}
+            increaseButtonTitle = {state.increaseButtonTitle}
+            max = {state.max}
+            min = {state.min}
             onChange = {state.onChange}
-            smallStep={state.step}
-            value={state.value}
+            smallStep = {state.step}
+            tickPlacement = {state.tickPlacement}
+            title = {state.title}
+            value = {state.value}
+            vertical
         />,
         document.getElementById('app')
     );
