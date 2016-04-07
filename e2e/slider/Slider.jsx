ddc496f663d9e661432dc66e7e39cc8fbfe3cbe5
Nikolay Rusev <nikolay.rusev@telerik.com>
2016-04-07 14:46:49 +0300

    feat(slider): add pressed state to slider handle

--- e2e/slider/Slider.jsx
+++ e2e/slider/Slider.jsx
@@ -4,55 +4,46 @@ import Slider from '../../src/slider/Slider';
 import { withRoot } from 'e2e-utils';
 
 describe('Slider', withRoot(root => {
-    function render() {
-        ReactDOM.render( <Slider {...props} />, root[0]);
-    }
-    let props;
-    it('should resize slider wrapper', () => {
-        props = {
+    function render(options) {
+        const props = Object.assign({
             max: 10,
             min: 0,
             style: { width: 300 },
-            step: 1
-        };
+            smallStep: 1,
+            value: 3,
+            onChange: () => { /*noop*/ }
+        }, options);
+
+        ReactDOM.render(<Slider {...props} />, root[0]);
+    }
+
+    it('should resize slider wrapper', () => {
         render();
+
         const slider = root.find('.k-slider');
+
         expect(slider.width()).toEqual(300);
     });
 
-    it('should resize  wrapper', () => {
-        props = {
-            max: 10,
-            min: 0,
-            style: { width: 300 },
-            step: 1
-        };
+    it('should resize wrapper', () => {
         render();
+
         const track = root.find('.k-slider-track');
+
         expect(track.width()).toEqual(230);
     });
 
     it('should add tabindex attributes', () => {
-        props = {
-            max: 10,
-            min: 0,
-            style: { width: 300 },
-            step: 1
-        };
         render();
+
         const handle = root.find('.k-draghandle');
+
         expect(handle.attr('tabindex')).toEqual('0');
     });
 
     it('should add aria attributes', () => {
-        props = {
-            max: 10,
-            min: 0,
-            style: { width: 300 },
-            step: 1,
-            value: 3
-        };
         render();
+
         const handle = root.find('.k-draghandle');
         const ariaMin = handle.attr('aria-valuemin');
         const ariaMax = handle.attr('aria-valuemax');
@@ -63,5 +54,66 @@ describe('Slider', withRoot(root => {
         expect(ariaMax).toEqual('10');
         expect(ariaNow).toEqual('3');
         expect(ariaText).toEqual('3');
+    });
+
+    it('should add k-pressed on track press', () => {
+        render();
+
+        const track = root.find('.k-slider-track')[0];
+        const handle = root.find('.k-draghandle');
+
+        track.dispatchEvent(new MouseEvent('mousedown', { bubbles: true }));
+
+        expect(handle.hasClass('k-pressed')).toBeTruthy();
+    });
+
+    it('should trigger change on track press', () => {
+        const changeSpy = jasmine.createSpy("onChange");
+        const props = {
+            min: 1,
+            onChange: changeSpy
+        };
+
+        render(props);
+
+        const track = root.find('.k-slider-track')[0];
+        const tick = root.find('.k-tick').first();
+
+        track.dispatchEvent(new MouseEvent('mousedown', {
+            bubbles: true,
+            clientX: tick.offset().left,
+            clientY: tick.offset().top
+        }));
+
+        expect(changeSpy).toHaveBeenCalledWith({ value: props.min });
+    });
+
+    it('should trigger change on handle drag', () => {
+        const changeSpy = jasmine.createSpy("onChange");
+        const props = {
+            min: 0,
+            max: 3,
+            value: 0,
+            onChange: changeSpy
+        };
+
+        render(props);
+
+        const track = root.find('.k-slider-track')[0];
+        const tick = root.find('.k-tick');
+
+        track.dispatchEvent(new MouseEvent('mousedown', {
+            bubbles: true,
+            clientX: tick.first().offset().left,
+            clientY: tick.first().offset().top
+        }));
+
+        document.dispatchEvent(new MouseEvent('mousemove', {
+            bubbles: true,
+            clientX: tick.eq(1).offset().left + 40,
+            clientY: tick.eq(1).offset().top
+        }));
+
+        expect(changeSpy).toHaveBeenCalledWith({ value: 1 });
     });
 }));
